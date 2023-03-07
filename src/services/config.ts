d9c0efa47c4af77d9d4cffd3c83d73e9a3ba6d7c
Daniela Lara <laradni@icloud.com>
2023-03-07 11:47:42 -0600

    (Add)

--- src/services/config.ts
+++ src/services/config.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   projectId: "vetsnpets-52c1f",
   storageBucket: "vetsnpets-52c1f.appspot.com",
   messagingSenderId: "800037039816",
-  appId: "1:800037039816:web:07111d37cc6e4c98377ffd",
+  appId: "1:800037039816:web:07111d37cc6e4c98377ffd"
 };
 
 // Initialize Firebase
